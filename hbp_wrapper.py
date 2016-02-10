1441bb2dc45c25d03925053e8268d1ba00101202
Anze Vavpetic <anze.vavpetic@ijs.si>
2016-02-10 14:02:18 +0100

    Adds wrappers and test data for the HBP medical platform.

---
+++ hbp_wrapper.py
@@ -0,0 +1,41 @@
+'''
+Wrapper for Hedwig to work within the HBP medical platform.
+
+Example:
+$ python hbp_wrapper.py test_data.json tsnr
+
+@author: anze.vavpetic@ijs.si
+'''
+
+import json
+import sys
+import tempfile
+from subprocess import call
+
+import preprocess
+
+
+if __name__ == '__main__':
+    in_file = sys.argv[1]
+    target_att = sys.argv[2]
+    out_file = sys.argv[3] if len(sys.argv) > 3 else 'out.csv'
+    rules_out_file = '{}.json'.format(out_file.split('.')[0])
+
+    with open(in_file) as f:
+        data = json.load(f)
+
+    matrix, attributes = preprocess.to_matrix(data, target_att)
+    binarized_data, new_attributes = preprocess.binarize(matrix, attributes)
+    preprocess.dump_to_csv(binarized_data, new_attributes, out_file)
+
+    # Call hedwig with sensible defaults
+    empty_bk = tempfile.mkdtemp()
+    call([
+        'python', '-m', 'hedwig',
+        empty_bk,
+        out_file,
+        '-f', 'csv',
+        '-l',
+        '-o', rules_out_file,
+        '-a', 'none'
+    ])
---
+++ preprocess.py
@@ -0,0 +1,56 @@
+import numpy as np
+from scipy import stats
+
+
+def binarize(matrix, attributes, bins=4):
+    ''' Use a simple binning method for continous attributes '''
+    target_att = attributes[-1]
+
+    # Compute bins for each attribute
+    new_attributes = []
+    binarized_data = [[] for _ in range(len(matrix))]
+    for i, att in enumerate(attributes):
+        _, edges, membership = stats.binned_statistic(matrix[:,i], matrix[:,i], bins=bins)
+
+        binned_attributes = []
+        if att == target_att:
+            # For the target attribute, use the bins as target values
+            binned_attributes.append(att)
+            target_values = []
+            for i, _ in enumerate(edges):
+                lower, upper = str(edges[i-1]), str(edges[i])
+                target_values.append('{}=<{}<{}'.format(lower,att,upper))
+
+            for j, bin_idx in enumerate(membership):
+                binarized_data[j].append(target_values[bin_idx-1])
+        else:
+            for i, _ in enumerate(edges):
+                lower, upper = str(edges[i-1]), str(edges[i])
+                binned_attributes.append('{}=<{}<{}'.format(lower,att,upper))
+
+            for i, _ in enumerate(binned_attributes):
+                for j, bin_idx in enumerate(membership):
+                    binarized_data[j].append('0' if bin_idx-1 != i else '1')
+
+        new_attributes = new_attributes + binned_attributes
+
+    return binarized_data, new_attributes
+
+
+def to_matrix(data, target_att):
+    ''' Converts the input json data to a numpy array and a list of attributes '''
+    attributes = filter(lambda key: key!=target_att, data[0].keys()) + [target_att]
+    data_list = []
+    for e in data:
+       data_list.append([e[att] for att in attributes])
+
+    return np.array(data_list), attributes
+
+
+def dump_to_csv(data, attributes, out_file):
+    ''' Output to csv for hedwig '''
+    with open(out_file, 'w') as f:
+        f.write('{}\n'.format(';'.join(['id'] + attributes)))
+
+        for id, example in enumerate(data):
+            f.write('{}\n'.format(';'.join([str(id)] + example)))
