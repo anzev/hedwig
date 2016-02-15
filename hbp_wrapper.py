57555b562f4ecc70705bcaf2557ea6ba5a3b3b60
Anze Vavpetic <anze.vavpetic@ijs.si>
2016-02-15 13:18:51 +0100

    Edit-in data specification information

--- hbp_wrapper.py
+++ hbp_wrapper.py
@@ -18,7 +18,7 @@ import preprocess
 if __name__ == '__main__':
     in_file = sys.argv[1]
     target_att = sys.argv[2]
-    out_file = sys.argv[3] if len(sys.argv) > 3 else 'out.csv'
+    out_file = sys.argv[3]
     rules_out_file = '{}.json'.format(out_file.split('.')[0])
 
     with open(in_file) as f:
@@ -29,14 +29,33 @@ if __name__ == '__main__':
     preprocess.dump_to_csv(binarized_data, new_attributes, out_file)
 
     # Call hedwig with sensible defaults
+    examples_file = out_file
+
     empty_bk = tempfile.mkdtemp()
     call([
         'python', '-m', 'hedwig',
         empty_bk,
-        out_file,
+        examples_file,
         '-f', 'csv',
         '-l',
         '-o', rules_out_file,
         '-a', 'none',
         '--nocache'
     ])
+
+    rules = {}
+    with open(rules_out_file) as f:
+        rules = json.load(f)
+
+    # Edit-in the dataSpecification data
+    for att in attributes:
+        att_data = {
+            'attributeName': att,
+            'attrbitueType': 'numeric'
+        }
+        rules['dataSpecification']['attributes'].append(att_data)
+    rules['dataSpecification']['task']['taskDescriptiveAttributes'] = attributes[:-1]
+
+    # Save changes
+    with open(rules_out_file, 'w') as f:
+        json.dump(rules, f, indent=2)
